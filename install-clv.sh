6723f3429043de5e8f5678b51ffc9c16243580d2
clavsiound-srv <korgie@gmail.com>
2022-01-04 01:46:45 +0200

    ertflix-d.clv: audio it's not static

--- install-clv.sh
+++ install-clv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cp usr/local/bin/*.clv /usr/local/bin/
-cp usr/local/sbin/*.clv /usr/local/sbin/
-cp etc/*.sed /etc/
+cp -p usr/local/bin/*.clv /usr/local/bin/
+cp -p usr/local/sbin/*.clv /usr/local/sbin/
+cp -p etc/*.sed /etc/
 chmod 755 /usr/local/*bin/*.clv
